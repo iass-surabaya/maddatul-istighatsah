2d8e5a225f9c59ef52882252027858c0ccc148f1
msukri89 <mochammadsyukri@gmail.com>
2026-05-10 18:53:47 +0700

    Update service worker cache version to v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maddatul-istighatsah-v14'; // Versi dinaikkan agar HP otomatis update
+const CACHE_NAME = 'maddatul-istighatsah-v15'; // Versi dinaikkan agar HP otomatis update
 const urlsToCache = [
   './',
   './index.html',
